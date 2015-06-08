ae057b77de04751f18576208c2afdeb61fdad914
Michael Wiss <michael.wiss@gmail.com>
2015-06-08 18:21:34 -0500

    sign in spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,5 +48,5 @@ end
 private
 
  def user_params
-	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	params.require(:user).permit(:name, :email, :password, :password_confirmation, :username)
  end
