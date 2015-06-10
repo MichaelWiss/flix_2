4f99c6830b3a2996c7c7febc94873942b20658be
Michael Wiss <michael.wiss@gmail.com>
2015-06-10 14:11:49 -0500

    authenticate by username

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
    end
 
    def self.authenticate(email, password)
-       user = User.find_by(email: email)
+       user = User.find_by(email: email_or_username) || User.find_by(username: email_or_username)
        user && user.authenticate(password)
    end
 end
