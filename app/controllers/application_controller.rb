2e2569a87d1bb4aaf851cbac83bfac4dc2690538
Michael Wiss <michael.wiss@gmail.com>
2015-06-16 15:07:21 -0500

    helper method added to movies controller and sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,12 @@ private
     end
   end
 
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Unauthorized access!"
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -23,6 +29,13 @@ private
   end
 
   helper_method :current_user?
+
+  def current_user_admin?
+     current_user && current_user.admin?
+  end
+
+  helper_method :current_user_admin?
+  
 end
 
 
