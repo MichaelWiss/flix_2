9403442ef89340069d82aa83c14b7d19d18eed74
Michael Wiss <michael.wiss@gmail.com>
2015-06-21 10:51:14 -0500

    added favorite associations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorit_movies, through: :favorites, source: :movie 
+
+
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true,
