1f34a06f7e16622ddcd8c1cbe6f80c4af108b4d0
Michael Wiss <michael.wiss@gmail.com>
2015-06-25 16:30:35 -0500

    added genres array to movies show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,6 +14,7 @@ class MoviesController < ApplicationController
     if current_user
       @current_favorite = current_user.favorites.find_by(movie_id: @movie.id)
     end
+    @genres = @movie.genres
   end
   
   def edit
