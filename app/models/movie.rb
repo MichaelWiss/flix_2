91ecac4f8245028200c11707c981c4a11858d17d
Michael Wiss <michael.wiss@gmail.com>
2015-06-28 18:30:09 -0500

    released on scope added to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -27,9 +27,7 @@ class Movie < ActiveRecord::Base
 
 
   
-  def self.released
-    where("released_on <= ?", Time.now).order("released_on desc")
-  end
+  scope :released, -> { where("released_on <= ?", Time.now).order(released_on: :desc) }
   
   def self.hits
     where('total_gross >= 300000000').order('total_gross desc')
