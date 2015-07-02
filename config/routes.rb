f4f1ae03fce791488489a3e8dc3dde25b7dcb95b
Michael Wiss <michael.wiss@gmail.com>
2015-07-01 21:12:42 -0500

    added scope to routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :users
   resource :session
 
- 
+ get "movies/filter/hits" => "movies#index", scope: "hits"
 
   root "movies#index"
   resources :movies do
